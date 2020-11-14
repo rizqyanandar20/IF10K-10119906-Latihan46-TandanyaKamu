d99042a40ae228efcb4ea95307eecb676b7e9787
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-11-14 22:46:41 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan46/tandanyakamu/IF10K10119906Latihan46TandanyaKamu.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan46.tandanyakamu;
+
+import java.time.Year;
+import java.util.Scanner;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama     : Rizqy Ananda Rusmana
+ * NIM      : 10119906
+ * Kelas    : IF-10K
+ */
+public class IF10K10119906Latihan46TandanyaKamu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        int tahunLahir, tahunSekarang, umur;
+        String normal = "\u001b[0m";
+        String merah = "\u001B[31m";
+
+        tahunSekarang = Year.now().getValue();
+        Age user = new Age(tahunSekarang);
+
+        System.out.print("Masukkan Tahun Lahir Anda : ");
+        user.setYearBirth(scanner.nextInt());
+        tahunLahir = user.getYearBirth();
+        umur = user.hitungUmur();
+
+        System.out.println("\n======Hasil Perhitungan Umur======");
+        System.out.println("Tahun lahir anda : "+ tahunLahir);
+        System.out.println("Hari ini tahun : "+tahunSekarang);
+        System.out.printf("Umur kamu sampai hari ini adalah %d tahun%n", umur);
+        System.out.println("Tandanya Kamu "+ merah+ user.tandanyaKamu(umur)+ normal);
+    }
+}
